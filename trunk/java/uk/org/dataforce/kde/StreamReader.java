37708e45056e8143390835edc7f5f050e5be4320
ShaneMcC <ShaneMcC@4bb5603a-c237-0410-82bd-99afdd509fdd>
2008-06-14 23:05:17 +0000

    Fix License Headers Fix Package names Fix opening the dialog in the home dir when not the CurrentDirectory if a relative filename has been specified

--- trunk/java/uk/org/dataforce/kde/StreamReader.java
+++ trunk/java/uk/org/dataforce/kde/StreamReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2007 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ * Copyright (c) 2006-2008 Shane Mc Cormack
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -18,6 +18,8 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
+ * SVN: $Id$
  */
 
 package uk.org.dataforce.kde;
